3b6cae764476e354436c355d29f34240803113a1
Franccesco Orozco <francc3sco@gmail.com>
2018-04-19 16:15:26 -0600

    remove success messages

--- tests/test_challenge_1.py
+++ tests/test_challenge_1.py
@@ -23,8 +23,6 @@ class TestModules(unittest.TestCase):
                         msg="The file __init__.py was not found in'modules/'")
         self.assertTrue(isfile(self.modules_dir + self.ip_class),
                         msg="The file ipinfo.py was not found in'modules/'")
-        print('Challenge #1 was a success!')
-        print('Please proceed with Challenge #2\n')
 
 
 if __name__ == '__main__':
