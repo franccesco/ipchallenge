1f711e8a85e8cd9fc56eed3385b41eabb238472f
Franccesco Orozco <francc3sco@gmail.com>
2018-04-19 15:01:15 -0600

    add one more assert to test_create_module_folder

--- tests/test_challenge_1.py
+++ tests/test_challenge_1.py
@@ -10,6 +10,7 @@ class TestModules(unittest.TestCase):
         """Set up class variables."""
         self.modules_dir = 'modules/'
         self._init = '__init__.py'
+        self.ip_class = 'ipinfo.py'
 
     def test_create_module_folder(self):
         """Test the project architecture"""
@@ -20,3 +21,5 @@ class TestModules(unittest.TestCase):
         # test if __init.py__ is under the 'modules/' directory.
         self.assertTrue(isfile(self.modules_dir + self._init),
                         msg="The file __init__.py was not found in'modules/'")
+        self.assertTrue(isfile(self.modules_dir + self.ip_class),
+                        msg="The file ipinfo.py was not found in'modules/'")
