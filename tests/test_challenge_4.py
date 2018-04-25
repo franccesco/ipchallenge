2423d03c71bb07b235fbb04e482318229e3bf633
Franccesco Orozco <francc3sco@gmail.com>
2018-04-24 20:13:58 -0600

    add iteration check in test 4

--- tests/test_challenge_4.py
+++ tests/test_challenge_4.py
@@ -1,6 +1,7 @@
 """Challenge #4."""
 
 import unittest
+from types import GeneratorType
 from modules import IPInfo
 
 
@@ -12,7 +13,13 @@ class TestIPInfoREPR(unittest.TestCase):
         self.test_ip = '8.8.8.8'
         self.ip_info = IPInfo(self.test_ip)
 
-    def test_repr(self):
+    def test_1_repr(self):
         """Test if repr have a custom message."""
         self.assertEqual(
             f'Geolocation information of {self.test_ip}', repr(self.ip_info))
+
+    def test_2_iter(self):
+        """Test iteration method."""
+        self.assertIsInstance(iter(self.ip_info), GeneratorType)
+        values = dict(val for val in self.ip_info)
+        self.assertIsInstance(values, dict)
