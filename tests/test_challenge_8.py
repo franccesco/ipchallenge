2805a87e6a566819f3bc6e0512808f3b2f36f2db
Franccesco Orozco <francc3sco@gmail.com>
2018-04-26 22:51:39 -0600

    add methods and arguments to challenge 8.

--- tests/test_challenge_8.py
+++ tests/test_challenge_8.py
@@ -18,12 +18,15 @@ class TestIPModules(unittest.TestCase):
 
     def test_1_jsonify(self):
         """Test if module Jsonify module returns a jsonified dictionary."""
-        self.assertTrue(loads(self.ip_info.jsonify()))
+        jsonified_str = self.ip_info.jsonify()
+        self.assertTrue(loads(jsonified_str))
+        self.assertEqual(len(jsonified_str.split('\n')), 1,
+                         msg="Your jsonify method is indenting by default.")
 
     def test_2_to_file(self):
         """Test if data is dumped to file."""
         # dump to a file and check if it was created
-        self.ip_info.to_file(self.file_dump, indent=4)
+        self.ip_info.to_file(self.file_dump)
         self.assertTrue(isfile(self.file_dump))
 
         # check if file json compatible and finally remove it
@@ -31,3 +34,15 @@ class TestIPModules(unittest.TestCase):
             json_data = robject.read()
         self.assertTrue(loads(json_data))
         remove(self.file_dump)
+
+    def test_3_identation(self):
+        """Test if jsonified data is indented."""
+        jsonified_str = self.ip_info.jsonify(indent=4)
+        self.assertIn(' ' * 4, jsonified_str)
+
+    def test_4_file_indentation(self):
+        """Test if file output is indented."""
+        self.ip_info.to_file(self.file_dump, indent=4)
+        with open(self.file_dump, 'r') as rdata:
+            jsonified_str = rdata.read()
+        self.assertIn(' ' * 4, jsonified_str)
